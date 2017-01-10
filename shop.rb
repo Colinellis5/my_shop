cfe4a4d831989a13073e495ee1cd7780934f18b5
Colinellis5 <colin@colinellis.nl>
2017-01-10 18:11:03 +0100

    added choose services

--- shop.rb
+++ shop.rb
@@ -1,12 +1,12 @@
-## the data
+## initialize the important variables
 
 @shopping_cart = []
 
 @services = [
-  { reference_number: 1, name: "Meet the family", price: 20 },
-  { reference_number: 2, name: "Full day with family", price: 50 },
+  { reference_number: 1, name: "Meet the family < 3 hours", price: 20 },
+  { reference_number: 2, name: "Full day with family", price: 100 },
   { reference_number: 3, name: "Drinks with friends", price: 10 },
-  { reference_number: 4, name: "Full day birthdays, weddings, etc from friends", price: 30 },
+  { reference_number: 4, name: "Birthdays, weddings, etc from friends (full day)", price: 30 },
   { reference_number: 5, name: "Christmas", price: 200 },
   { reference_number: 6, name: "Family weekend", price: 300 },
 ]
@@ -27,17 +27,26 @@ def welcome_message
   print_divider
 end
 
-## Show the menu
+# Show the menu
 
-services.each_with_index do |service, index|
-    puts (index+1).to_s + ") " + service[:description]
+def show_menu
+puts "here are all my services"
+  @services.each do |service|
+  puts "#{service[:reference_number]}, #{service[:name]} -- costs #{service[:price]}"
+  end
 end
 
 ## user actions
 
 def choose_service
-  puts "let's select a product, choose " + services.each_key { |key| puts "#{reference_number}, "}
-  add_to_shopping_cart
-  update_total
-
+  puts "To select a product, choose a number from the list"}
+  gets.chomp.to_1
 end
+
+
+
+## Iterating through the program steps
+
+welcome_message
+show_menu
+choose_service
